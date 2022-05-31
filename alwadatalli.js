4704a73b2466b26535315784ebdd1f970d5eb137
judithlk <104207025+judithlk@users.noreply.github.com>
2022-05-31 08:39:37 +0100

    Initial commit

---
+++ alwadatalli.js
@@ -0,0 +1,7 @@
+function showForm(theId) {
+    document.getElementById(theId).style.display = "block";
+};
+
+function cancelShow(theId) {
+    document.getElementById(theId).style.display = "none";
+};
